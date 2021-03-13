b453582d44fed4cdd1dab312bdedd1030cf3ec50
DarrenLiu <45053964+ntlnr@users.noreply.github.com>
2021-03-13 00:07:04 -0600

    Update hashtable.h

--- ProgAssignemnt1/hashtable.h
+++ ProgAssignemnt1/hashtable.h
@@ -23,11 +23,9 @@ typedef struct Hash {
 
 void displayMenu();
 HashNode* initHashNode(int key, int hashIndex, char* val);
-FriendNode* initFriendNode(char* name);
+FriendNode* initFriendNode(char* friendname);
 HashNode* locateHash(char* target, Hash* hashTable, int size);
-FriendNode* locateFriend(HashNode * node, char *friendName);
-void deleteFriend(HashNode* Node, FriendNode* Friend);
-void connectFriend(HashNode* Node, FriendNode* Friend);
+void deleteFriend(HashNode* node, char* FriendName);
+void connectFriend(HashNode* node, FriendNode* Friend);
 int keyHelper(char* val);
-
 
